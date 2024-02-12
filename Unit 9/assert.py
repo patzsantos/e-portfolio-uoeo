950b5771e3265821e4c45226a8b6a677c0f97db4
Patricia Santos <141705254+patzsantos@users.noreply.github.com>
2024-02-12 23:52:12 +0800

    Add files via upload

---
+++ Unit 9/assert.py
@@ -0,0 +1,22 @@
+# Python String Operations with Assertions
+
+str1 = 'Hello'
+str2 = 'World!'
+
+# using +
+concatenation = str1 + str2
+print('str1 + str2 = ', concatenation)
+assert concatenation == 'HelloWorld!', "Wrong. Expected: 'Hello World!"
+
+# using *
+multiplication = str1 * 3
+print('str1 * 3 =', multiplication)
+assert multiplication == 'HelloHelloHello', "Wrong. Expected: 'HelloHelloHello'"
+
+
+
+
+
+# References
+#   W3Schools. (N.D.) Python assert Keyword. Available from:
+#   https://www.w3schools.com/python/ref_keyword_assert.asp [Accessed 23 January 2024].
