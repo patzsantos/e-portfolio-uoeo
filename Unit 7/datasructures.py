d0c47a2ffe1ba7abe02b75ba557030eb19b3feb2
Patricia Santos <141705254+patzsantos@users.noreply.github.com>
2024-02-12 23:23:10 +0800

    Add files via upload

---
+++ Unit 7/datasructures.py
@@ -0,0 +1,16 @@
+# 1 dictionary of traffic light colors and what their signals mean
+trafficLight = {
+    "red": "stop",
+    "yellow": "slow",
+    "green": "go"
+}
+print(trafficLight)
+
+
+# 2 examples of lists that can store car control motions and objects to avoid
+carControl = ["steer left", "steer right", "steer straight", "accelerate", "brake"]
+print(carControl)
+
+
+avoidObject = ["human", "animal", "big rock", "another car"]
+print(avoidObject)
