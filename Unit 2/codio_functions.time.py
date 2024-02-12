077308a3efaf87082e04ac307a16bee06e66b908
Patricia Santos <141705254+patzsantos@users.noreply.github.com>
2024-02-12 20:57:37 +0800

    Update codio_functions.time.py

--- Unit 2/codio_functions.time.py
+++ Unit 2/codio_functions.time.py
@@ -1,5 +1,8 @@
 class Time:
+     """Represents the time of day.
 
+    attributes: hour, minute, second
+    """
     def __init__(self, hour=0, minute =0, second =0):
         self.hour = hour
         self.minute = minute
