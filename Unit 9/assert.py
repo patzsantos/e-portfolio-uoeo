65d0bfbd619e7ca1f1164a10f3aea8cf1032552b
Patricia Santos <141705254+patzsantos@users.noreply.github.com>
2024-02-12 23:54:04 +0800

    Update assert.py

--- Unit 9/assert.py
+++ Unit 9/assert.py
@@ -1,3 +1,6 @@
+"""Activity 4: Extend the following program to test accuracy of operations using the assert statement."""
+
+
 # Python String Operations with Assertions
 
 str1 = 'Hello'
@@ -16,7 +19,6 @@ assert multiplication == 'HelloHelloHello', "Wrong. Expected: 'HelloHelloHello'"
 
 
 
-
-# References
-#   W3Schools. (N.D.) Python assert Keyword. Available from:
-#   https://www.w3schools.com/python/ref_keyword_assert.asp [Accessed 23 January 2024].
+"""References
+W3Schools. (N.D.) Python assert Keyword. Available from:
+https://www.w3schools.com/python/ref_keyword_assert.asp [Accessed 23 January 2024]."""
