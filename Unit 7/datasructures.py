a7e092d35838e75b075af5bf3d71ed41811cdbeb
Patricia Santos <141705254+patzsantos@users.noreply.github.com>
2024-02-17 14:37:48 +0800

    Update datasructures.py

--- Unit 7/datasructures.py
+++ Unit 7/datasructures.py
@@ -1,29 +1,30 @@
 class Car:
-    def __init__(self, make, model, year): 
+    def __init__(self, make, model, year):
         self.make = make
         self.model = model
         self.year = year
         self.data = {
-            "make" : self.make, 
-            "model" : self.model,
-            "year": self.year, 
+            "make": self.make,
+            "model": self.model,
+            "year": self.year,
             "traffic_light": {
-                "red": "stop", 
+                "red": "stop",
                 "yellow": "slow",
                 "green": "go"
             },
-            "car_control": ["steer left", "steer right", "steer straight", "accelerate", "brake"], 
+            "car_control": ["steer left", "steer right", "steer straight", "accelerate", "brake"],
             "avoid_object": ["human", "animal", "big rock", "another car"]
         }
 
-        def items(self):
-            return self.data.items()
+    def items(self):
+        return self.data.items()
 
-        def keys(self): 
-            return self.data.keys()
+    def keys(self):
+        return self.data.keys()
 
-        def values(self):
-            return self.data.values()
+    def values(self):
+        return self.data.values()
+
 
 my_car = Car("Mazda", "CX 5", 2019)
 
@@ -33,5 +34,5 @@ print(my_car.items())
 print("\nKeys:")
 print(my_car.keys())
 
-print("n\Values:")
+print("\nValues:")
 print(my_car.values())
